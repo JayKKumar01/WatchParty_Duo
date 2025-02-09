7bde11e7519b33a36df4300eb764c9c7a9632156
Jay Kumar <jaytechbc@gmail.com>
2025-02-09 18:36:54 +0530

    synchronized audio pretty well

--- app/src/main/java/com/github/jaykkumar01/watchparty_duo/imagefeed/CameraSessionManager.java
+++ app/src/main/java/com/github/jaykkumar01/watchparty_duo/imagefeed/CameraSessionManager.java
@@ -56,7 +56,7 @@ public class CameraSessionManager implements ImageReader.OnImageAvailableListene
                 previewSize.getWidth(),
                 previewSize.getHeight(),
                 ImageFormat.YUV_420_888,
-                2
+                5
         );
         imageReader.setOnImageAvailableListener(this, null);
     }
